2b43fde89b396aa81060efff0e3785919020c745
Hidetchi <hidetchi@81dojo.com>
2014-01-04 12:12:39 +0100

    Changed design of round-robin table.

--- templates/group_template.php
+++ templates/group_template.php
@@ -2,21 +2,34 @@
 
 <head>
 <style type="text/css">
-  table, td, th { border: 1px solid; }
-  td { width: 60px; }
-  td.void { background-color: rgb(153, 153, 153);  }
-  td.small { font-size:small; }
-  td.center { text-align:center }
+  table {
+    border:2px solid #666;
+    border-collapse: collapse;
+    font-family: Verdana;
+    font-size: 14px;
+    box-shadow: 3px 3px 12px rgba(0,0,0,0.3);
+    text-align: center;
+  }
+  tr.header { background: linear-gradient(to bottom,#ddd,#eee,#ccc); }
+  th { width:30px; padding: 6px 3px; border: 1px solid #666; }
+  td { width:30px; height: 22px; padding-left: 3px; padding-right: 3px; border: 1px solid #666; }
+  td.void { background-color: rgb(153, 153, 153); }
+  td.mark { padding: 0; }
+  th.small { font-size:small; }
+  td.left { text-align:left; }
+  td.colored { background: #ffe; }
+  td.mark a { display: block; height: 22px; line-height: 22px; text-decoration: none; color: black; font-size: 16px; }
+  td.mark a:hover { background: #8f8; color: red;}
 </style>
 </head>
 
 <body>
-<table style="border:2px solid;">
-  <tr>
+<table>
+  <tr class="header">
     <th>#</th>
     <th>Name</th>
     <th class="small">Country</th>
-    <th class="small">Total<br/>Points</th>
+    <th class="small">Points</th>
     <th class="small">Rank</th>
     <?php for($i=0;$i<count($players);$i++): ?>
       <th><?php echo ($i+1); ?></th>
@@ -25,17 +38,17 @@
   <tr>
   <?php for($i=0;$i<count($players);$i++): ?>
     <tr>
-      <td class="center"><?php echo ($i+1); ?></td>
-      <td><?php echo htmlspecialchars($players[$i]->name); ?></td>
-      <td class="center"><?php echo $players[$i]->country_flag_s_tag(); ?></td>
-      <td class="center"><?php echo $players[$i]->calculate_point(); ?></td>
-      <td class="center"><?php echo Util::ordinalize($player_ranks[$players[$i]->name]+1); ?></td>
+      <td><?php echo ($i+1); ?></td>
+      <td class="left"><?php echo htmlspecialchars($players[$i]->name); ?></td>
+      <td><?php echo $players[$i]->country_flag_s_tag(); ?></td>
+      <td class="colored"><?php echo $players[$i]->calculate_point(); ?></td>
+      <td class="colored"><?php echo Util::ordinalize($player_ranks[$players[$i]->name]+1); ?></td>
       <?php for($j=0;$j<count($players);$j++): ?>
         <?php if($i == $j): ?>
           <td class="void">&nbsp;</td>
         <?php else: ?>
           <?php if($result = $players[$i]->get_result($players[$j])): ?>
-            <td class="center">
+            <td class="mark">
               <a target="_blank" href="http://81dojo.com/kifuviewer.html?kid=<?php echo $result->kifu_id; ?>"><?php echo $result->result_label_for($players[$i]); ?></a>
             </td>
           <?php else: ?>
@@ -43,7 +56,7 @@
           <?php endif ?>
         <?php endif; ?>
       <?php endfor; ?>
-      <td class="center"><?php echo isset($players[$i]->comment) ? htmlspecialchars($players[$i]->comment) : '&nbsp;' ?></td>
+      <td class="colored"><?php echo isset($players[$i]->comment) ? htmlspecialchars($players[$i]->comment) : '&nbsp;' ?></td>
     </tr>
   <?php endfor; ?>
 </table>
